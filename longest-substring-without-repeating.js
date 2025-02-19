4fe70c7c7b62cec98e7edf9188b4bcf375acc107
Halanau <a.halanau@gmail.com>
2025-02-19 10:40:58 +0300

    add task: longest-sub-with-rep

--- longest-substring-without-repeating.js
+++ longest-substring-without-repeating.js
@@ -3,7 +3,30 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    return new Set(s).size
+    const charSet = new Set()
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++
+        }
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+    return maxLength;
 };
 
-lengthOfLongestSubstring("pwwkew")
+lengthOfLongestSubstring("pwwkewah")
+
+// p
+// pw
+// w
+// wk
+// wke
+// kew
+// kewa
+// kewah
+
+// maxLength:kewah = 5
