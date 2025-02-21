aec5c6727f92c0d84b96d0c15f2ce0c6bfad5a25
Halanau <a.halanau@gmail.com>
2025-02-21 11:48:34 +0300

    add task: intersection-of-two-arr.js

--- intersection-of-two-arr.js
+++ intersection-of-two-arr.js
@@ -7,22 +7,21 @@ var intersect = function (nums1, nums2) {
     let sortedNums1 = nums1.sort((a, b) => a - b)
     let sortedNums2 = nums2.sort((a, b) => a - b)
     let newArr = []
+    let k = 0
+    let m = 0
 
-    console.log(sortedNums1)
-    console.log(sortedNums2)
-
-    let minLength = sortedNums1.length
-    if (sortedNums1.length > sortedNums2.length) {
-        minLength = sortedNums2.length
-    }
-
-    for (let i = 0; i < minLength; i++) {
-        console.log(minLength)
-        if (sortedNums1[i] === sortedNums2[i]) {
-            newArr.push(sortedNums1[i])
+    while (k < sortedNums1.length && m < sortedNums2.length) {
+        if (sortedNums1[k] === sortedNums2[m]) {
+            newArr.push(sortedNums1[k]);
+            k++;
+            m++;
+        } else if (sortedNums1[k] < sortedNums2[m]) {
+            k++;
+        } else {
+            m++;
         }
     }
-    return newArr
 
+    return newArr;
 };
 intersect([2, 5, 3, 6, 7, 2, 2], [21, 25, 3, 4, 11, 1, 2, 2])
